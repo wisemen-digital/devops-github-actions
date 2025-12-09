a53f207ad83713a79587e8b74f447605ee7e7798
David Jennes <david.jennes@wisemen.digital>
2025-12-09 18:41:51 +0100

    Improve test reporting

--- .github/actions/project/test-stack/scripts/nuxt.sh
+++ .github/actions/project/test-stack/scripts/nuxt.sh
@@ -2,15 +2,29 @@
 
 source "${BASH_SOURCE%/*}/_common.sh"
 
-if_not_debug export TEST_REPORTER="--reporter=junit --outputFile=${APP}-test-report-junit.xml"
-
 pushd_app
 if test -f '.env.test'; then
   cp -f '.env.test' '.env'
 fi
 touch .env
 
+# Run Tests
+
 start_log_group "Run Nuxt tests"
-pnpm nuxi prepare
-pnpm vitest --run $TEST_REPORTER
+(
+  JUNIT_REPORT_FILE=${APP}-test-report-junit.xml
+  TEST_REPORTER_OPTIONS=()
+  if_not_debug && TEST_REPORTER_OPTIONS+=(
+    --reporter=junit
+    --outputFile=$JUNIT_REPORT_FILE
+  )
+
+  pnpm nuxi prepare
+  pnpm vitest --run "${TEST_REPORTER_OPTIONS[@]}"
+
+  # Fix node test reporter not setting a 'testsuites.name' (VITEST_JUNIT_SUITE_NAME is obsolete)
+  if [ -f "$JUNIT_REPORT_FILE" ]; then
+    sed -i "s/name=\"vitest tests\"/name=\"${APP} Tests\">/" "$JUNIT_REPORT_FILE"
+  fi
+)
 stop_log_group
